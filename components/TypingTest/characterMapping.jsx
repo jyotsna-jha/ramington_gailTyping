6bdba38e17ac5377180fda3f9bca06c0e3b4f10f
jyotsna-jha <116620336+jyotsna-jha@users.noreply.github.com>
2023-11-25 09:34:37 +0545

    alt codes solved

--- components/TypingTest/characterMapping.jsx
+++ components/TypingTest/characterMapping.jsx
@@ -45,7 +45,7 @@ const characterMapping = {
   "[": "ख्",
   "]": "द्व",
   "\\": ")",
-  "~":"द्य",
+  "~": "द्य",
   z: "्र",
   Z: "र्र् ",
   x: "ग",
@@ -95,9 +95,57 @@ const characterMapping = {
   "{": "क्ष",
   "ॅ": "ँ",
   "ं": "",
- 
-  // ... (rest of the mappings)
+  "\u0021": "!", // Alt+033 mapped to "!"
+  "\u0022": '"', // Alt+034 mapped to "
+  "\u0023": "#", // Alt+035 mapped to #
+  "\u0024": "$", // Alt+036 mapped to $
+  "\u0025": "%", // Alt+037 mapped to %
+  "\u0026": "&", // Alt+038 mapped to &
+  "\u0027": "'", // Alt+039 mapped to '
+  "\u0028": "(", // Alt+040 mapped to (
+  "\u0029": ")", // Alt+041 mapped to )
+  "\u002A": "*", // Alt+042 mapped to *
+  "\u002B": "+", // Alt+043 mapped to +
+  "\u002C": ",", // Alt+044 mapped to ,
+  "\u002D": "-", // Alt+045 mapped to -
+  "\u002E": ".", // Alt+046 mapped to .
+  "\u002F": "/", // Alt+047 mapped to /
+  "\u003A": ":", // Alt+058 mapped to :
+  "\u003B": ";", // Alt+059 mapped to ;
+  "\u003C": "<", // Alt+060 mapped to <
+  "\u003D": "=", // Alt+061 mapped to =
+  "\u003E": ">", // Alt+062 mapped to >
+  "\u003F": "?", // Alt+063 mapped to ?
+  "\u0040": "@", // Alt+064 mapped to @
+  "\u005C": "\\", // Alt+092 mapped to \
+  "\u005D": "]", // Alt+093 mapped to ]
+  "\u005E": "^", // Alt+094 mapped to ^
+  "\u005F": "_", // Alt+095 mapped to _
+  "\u0060": "`", // Alt+096 mapped to `
+  "\u007B": "{", // Alt+0123 mapped to {
+  "\u007C": "|", // Alt+0124 mapped to |
+  "\u007D": "}", // Alt+0125 mapped to }
+  "\u0082": ",", // Alt+0147 mapped to ,
+  "\u0084": '"', // Alt+0148 mapped to "
+  "\u0085": "•", // Alt+0149 mapped to •
+  "\u0096": "–", // Alt+0150 mapped to –
+  "\u0097": "—", // Alt+0151 mapped to —
+  "\u0098": "~", // Alt+0152 mapped to ~
+  "\u0099": "™", // Alt+0153 mapped to ™
+  "\u009A": "š", // Alt+0154 mapped to š
+  "\u009B": "›", // Alt+0155 mapped to ›
+  "\u009C": "œ", // Alt+0156 mapped to œ
+  "\u00A9": "©", // Alt+0169 mapped to ©
+  "\u00B4": "´", // Alt+0184 mapped to ´
+  "\u00B9": "¹", // Alt+0185 mapped to ¹
+  "\u00BD": "½", // Alt+0189 mapped to ½
+  "\u00BE": "¾", // Alt+0190 mapped to ¾
+  "\u00D7": "×", // Alt+0215 mapped to ×
+  "\u00D8": "Ø", // Alt+0216 mapped to Ø
+  "\u00DE": "Þ", // Alt+0222 mapped to Þ
+  "\u00DF": "ß", // Alt+0223 mapped to ß
+  "\u00F7": "÷", // Alt+0247 mapped to ÷
+  
 };
 
-// ... (add more mappings as needed)
 export default characterMapping;
